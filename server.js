21b2d897dbd3e4cbf7f732c208e6db42408d10f7
root <root@localhost.localdomain>
2020-01-30 12:23:14 +0200

    Express server setup with initial routes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app= express();
+
+// Connect to mongo DB
+connectDB();
 
 app.get('/',  (req ,res) => res.json({msg: 'Welcome to be-youtiful-nales API...'}));
 
