d6806c60244e8f34606bf566595d70cd111d4289
root <root@localhost.localdomain>
2020-02-16 04:42:47 +0200

    contact us form hooked up to the database

--- be-youtiful/src/components/pages/Contact.js
+++ be-youtiful/src/components/pages/Contact.js
@@ -1,11 +1,13 @@
-import React,{useState} from  'react';
+import React,{useState, useContext} from  'react';
 import axios from 'axios';
+import AlertContext from '../../context/alert/alertContext'
 import { Card, Icon, Image,Form, Button , Message, Segment, Grid, Divider} from 'semantic-ui-react'
 
 
 
 const About = () => {
-
+  const alertContext = useContext(AlertContext)
+  const {setAlert} = alertContext
 const [query, setQuery] =useState({
   name:'',
   email:'',
@@ -17,14 +19,17 @@ const {name, email, message, cellphone} = query;
 
 const handleChange = (e) =>setQuery({...query, [e.target.name]: e.target.value});
 
-const sendQuery = async(q) =>{
+const sendQuery = async(e,q) =>{
+e.preventDefault();
   try {
-    const res =  await axios.post('/api/queries',q);
-      console.log(res.data.data)
+     await axios.post('/api/queries',q);
+      setAlert('Your Query has be submitted', 'danger')
   } catch (error) {
     console.log(error.message)
   }
 }
+
+
 
     return(
       <Segment pilled>
@@ -53,7 +58,7 @@ const sendQuery = async(q) =>{
 
 
  <Grid.Column  >      
-<Form fluid style={{marginTop:90}} onSubmit={() =>sendQuery(query)}>
+<Form fluid style={{marginTop:90}} onSubmit={(e) =>sendQuery(e,query)}>
 <Form.Group widths='equal' block>
   <Form.Input fluid label='First name' name="name" value={name} placeholder='Full Name'onChange={handleChange}/>
   <Form.Input fluid label='Email' name="email" value={email} placeholder='E-mail' onChange={handleChange} />
